a65148959718d39388de5e2d94f73eb0e9004577
prabhaav <prabhaav.bhardwaj@gmail.com>
2019-01-27 13:04:04 -0800

    integrating gun

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var port = 80;
+var Gun = require('gun');
+
+app.use(Gun.serve);
+app.use(express.static(__dirname));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var server = app.listen(port);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +35,17 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+var gun = Gun({
+  file: 'data.json', // local testing and development,
+  web: server,
+  s3: {
+    key: process.env.AWS_ACCESS_KEY_ID, // AWS Access Key
+    secret: process.env.AWS_SECRET_ACCESS_KEY, // AWS Secret Token
+    bucket: process.env.AWS_S3_BUCKET, // The bucket you want to save into
+    region: process.env.AWS_REGION
+  }
 });
 
 module.exports = app;
