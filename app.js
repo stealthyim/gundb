2235723e6d56e1f1757e78cd702bf35fe32c3ed2
prabhaav <prabhaav.bhardwaj@gmail.com>
2019-01-27 13:22:00 -0800

    gun cleanup

--- app.js
+++ app.js
@@ -1,45 +1,12 @@
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-
-var app = express();
-
 var port = 80;
 var Gun = require('gun');
-
+var app    = express();
 app.use(Gun.serve);
 app.use(express.static(__dirname));
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-
 var server = app.listen(port);
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 var gun = Gun({
   file: 'data.json', // local testing and development,
   web: server,
@@ -50,5 +17,9 @@ var gun = Gun({
     region: process.env.AWS_REGION
   }
 });
+
+global.Gun = Gun;
+
+console.log('Server started on port ' + port + ' with /gun');
 
 module.exports = app;
