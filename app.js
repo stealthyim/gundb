7c4ef385f80a4f9d7e6cd8f964ce4451f3ddf7e8
prabhaav <prabhaav.bhardwaj@gmail.com>
2019-01-27 23:41:09 -0800

    trying http config

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 require('dotenv').config();
+console.log('New HTTP config loaded');
+;(function(){
+	var cluster = require('cluster');
+	if(cluster.isMaster){
+	  return cluster.fork() && cluster.on('exit', function(){ cluster.fork() });
+	}
 
-var express = require('express');
-var port = process.env.GUN_PORT || 5444;
-var Gun = require('gun');
-var app = express();
+	var fs = require('fs');
+	var config = { port: process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.GUN_PORT || process.argv[2] || 8765 };
+	var Gun = require('gun')
 
-app.use(Gun.serve);
-app.use(express.static(__dirname));
+	if(process.env.HTTPS_KEY){
+		config.key = fs.readFileSync(process.env.HTTPS_KEY);
+		config.cert = fs.readFileSync(process.env.HTTPS_CERT);
+		config.server = require('https').createServer(config, Gun.serve(__dirname));
+	} else {
+		config.server = require('http').createServer(Gun.serve(__dirname));
+	}
 
-var server = app.listen(port);
+	var gun = Gun({
+    web: config.server.listen(config.port),
+    s3: {
+      key: process.env.AWS_ACCESS_KEY_ID, // AWS Access Key
+      secret: process.env.AWS_SECRET_ACCESS_KEY, // AWS Secret Token
+      bucket: process.env.AWS_S3_BUCKET, // The bucket you want to save into
+      region: process.env.AWS_REGION
+    }
+  });
 
-var gun = Gun({
-  file: 'data.json', // local testing and development,
-  web: server,
-  s3: {
-    key: process.env.AWS_ACCESS_KEY_ID, // AWS Access Key
-    secret: process.env.AWS_SECRET_ACCESS_KEY, // AWS Secret Token
-    bucket: process.env.AWS_S3_BUCKET, // The bucket you want to save into
-    region: process.env.AWS_REGION
-  }
-});
-
-console.log('\n\n\n\n****************************************\n\n');
-console.log('GUNNNNNNNNNNN Server started on port ' + port + ' with /gun');
-
-module.exports = app;
+	console.log('Relay peer started on port ' + config.port + ' with /gun');
+}());
